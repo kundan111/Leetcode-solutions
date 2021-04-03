af0df68166c4b19649dd4afe0292ffdd0dd33e06
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2021-04-03 16:14:43 +0530

    Time: 4 ms (71.09%), Space: 9.3 MB (32.51%) - LeetHub

--- longest-common-prefix/longest-common-prefix.cpp
+++ longest-common-prefix/longest-common-prefix.cpp
@@ -1,72 +1,83 @@
 class Solution {
 
 public:
-    bool static comparator(string s1, string s2)
+    pair<string,int> returnSmallestStringAndItsLength(vector<string> arr)
     {
-        return s1.size() < s2.size();
-    }
-    
-    vector<string> custom_sorter(vector<string>& arr)
-    {
-        sort(arr.begin(), arr.end(), comparator);
-        return arr;
+            pair<string,int> returnVal;
+            int i = 0;
+            int sz = arr.size();
+            int minLength = INT_MAX;
+            string smallestString = arr[0];
+            while(i < sz)
+            {
+                if(arr[i].size() < minLength)
+                {
+                    minLength = arr[i].size();
+                    smallestString = arr[i];
+                }
+                i++;
+            }
+
+            returnVal = make_pair(smallestString,minLength);
+            return returnVal;
     }
     
     string longestCommonPrefix(vector<string>& arr) {
         int arr_size = arr.size();
-        if(arr_size == 0)
+    if(arr_size == 0)
+    {
+        return "";
+    }
+
+    if(arr_size == 1)
+    {
+        return arr[0];
+    }
+
+    // sort the array in ascending order of size of string    
+    // arr = custom_sorter(arr); //nlog(n)
+
+    pair<string,int> res = returnSmallestStringAndItsLength(arr);
+    
+
+    bool breakItHere = true;
+
+
+    
+    // take the smallest size 1.e first element
+    string smallestString = res.first;
+    int smallestStringLength = res.second;
+
+
+    //return lcp
+    string lcp = "";
+
+    for(int i = 0; i < smallestStringLength; i++)
+    {
+        char cur_char = smallestString[i];
+
+        // Now check for this character in every other strings in the array
+        int j = 0;
+        while(j < arr_size)
         {
-            return "";
-        }
-        
-        
-        arr = custom_sorter(arr); //nlog(n)
-        
-
-        bool breakItHere = true;
-        
-        
-
-        if(arr_size == 1)
-        {
-            return arr[0];
-        }
-
-
-        // take the smallest size 1.e first element
-        string smallestString = arr[0];
-        int smallestStringLength = arr[0].size();
-
-
-        //return lcp
-        string lcp = "";
-
-        for(int i = 0; i < smallestStringLength; i++)
-        {
-            char cur_char = smallestString[i];
-
-            // Now check for this character in every other strings in the array
-            int j = 1;
-            while(j < arr_size)
+            if(cur_char != arr[j][i])
             {
-                if(cur_char != arr[j][i])
-                {
-                    breakItHere = false;
-                    break;
-                }
-                j++;
-            }
-
-            if(!breakItHere)
-            {
+                breakItHere = false;
                 break;
             }
-
-            lcp += cur_char;
+            j++;
         }
 
+        if(!breakItHere)
+        {
+            break;
+        }
+
+        lcp += cur_char;
+    }
 
 
-        return lcp;
+    
+    return lcp;
     }
 };
