0c623538859bd8fc8e46b3959c37371168963275
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2021-05-25 12:43:45 +0530

    Time: 36 ms (76.23%), Space: 37.2 MB (54.08%) - LeetHub

--- two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
+++ two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -1,46 +1,33 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-
 class Solution {
 public:
     bool findTarget(TreeNode* root, int k) {
-        unordered_map<int,int> m;
-        bool res = false;
-        if(!root)
-            return false;
-        
-        preOrder(root,m, k,res);
-        
-        return res;
+        vector<int> nums;
+        inorder(root, nums);
+        return findTargetInSortedArray(nums, k);
     }
-    
-    void preOrder(TreeNode* root,unordered_map<int,int> &m, int k, bool &res)
-    {
-        if(root)
-        {
-            m[root->val]++;
-            
-            if(( k - root->val != root->val  && m.find(k-(root->val)) != m.end()) || (k - root->val == root->val &&  m[root->val] > 1))
-            {
-                res = true;
-                return;
+
+private:
+    void inorder(TreeNode* node, vector<int>& nums) {
+        if (!node) return;
+        inorder(node->left, nums);
+        nums.push_back(node->val);
+        inorder(node->right, nums);
+    }
+
+    bool findTargetInSortedArray(vector<int> a, int target) {
+        for (int i = 0, j = a.size() - 1; i < j;) {
+            int sum = a[i] + a[j];
+            if (sum == target) {
+                return true;
             }
-            
-            preOrder(root->left,m,k,res);    
-            
-            preOrder(root->right,m,k,res);    
-            
+            else if (sum < target) {
+                i++;
+            }
+            else {
+                j--;
+            }
         }
+
+        return false;
     }
-    
-    
 };
