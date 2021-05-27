a76707903706f6f6e24cbcb9f319713b4eb95e7a
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2021-05-27 14:24:03 +0530

    Time: 8 ms (98.39%), Space: 23.7 MB (27.24%) - LeetHub

--- binary-tree-tilt/binary-tree-tilt.cpp
+++ binary-tree-tilt/binary-tree-tilt.cpp
@@ -9,24 +9,32 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+auto speed=[]()
+{
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
 class Solution {
 public:
+    int result = 0;
     int findTilt(TreeNode* root) {
         if(!root)
             return 0;
-        int res = 0;
-        subTreeSum(root,res);
         
-        return res;
+        subTreeSum(root);
+        
+        return result;
     }
     
-    int subTreeSum(TreeNode* root, int &result)
+    int subTreeSum(TreeNode* root)
     {
         if(!root)
             return 0;
         
-        int l_sum = subTreeSum(root->left,result);
-        int r_sum = subTreeSum(root->right,result);
+        int l_sum = subTreeSum(root->left);
+        int r_sum = subTreeSum(root->right);
         
         result = result + abs(l_sum - r_sum);
         
