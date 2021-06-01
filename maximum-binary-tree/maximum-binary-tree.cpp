b36e272c236dfbcb070508a3c5e7437ddf548249
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2021-06-01 13:30:49 +0530

    Time: 124 ms (20.75%), Space: 42.2 MB (69.91%) - LeetHub

--- maximum-binary-tree/maximum-binary-tree.cpp
+++ maximum-binary-tree/maximum-binary-tree.cpp
@@ -9,69 +9,21 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// This is O(n^2) time solution --> NOT OPTIMAL
+// Space: O(logn)
 class Solution {
+    using It = std::vector<int>::const_iterator;
+    
+    TreeNode* traverse(It from, It to) {
+        auto it = std::max_element(from, to);
+        if (it == to) 
+            return nullptr;
+        return new TreeNode(*it, traverse(from, it), traverse(it + 1, to));
+    }
+    
 public:
     TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
-        
-        int sz = nums.size();
-        int maxIndx = findMaxEleIndx(nums,0,sz-1);
-        int l = 0;
-        int r = sz-1;
-        
-        return treeBuilder(nums,0,sz-1,maxIndx,sz);
-    
-    }
-    
-    
-    
-    TreeNode* treeBuilder(vector<int>& nums , int  l  , int r, int maxIndx, int sz)
-    {
-        if(l > r)
-            return NULL;
-        if(l==r)
-        {
-            TreeNode* nn = new TreeNode(nums[l]);
-            return nn;
-        }
-        
-        TreeNode* root = new TreeNode(nums[maxIndx]);
-        
-        int l_maxIndx = findMaxEleIndx(nums, l, maxIndx-1);
-        int r_maxIndx = findMaxEleIndx(nums, maxIndx+1,r);
-
-        if(l_maxIndx != -1)
-        {
-            root->left = treeBuilder(nums,l,maxIndx-1,l_maxIndx,sz);    
-        }
-        
-        if(r_maxIndx != -1)
-        {
-            root->right = treeBuilder(nums,maxIndx+1,r,r_maxIndx,sz);    
-        }
-        
-        
-        return root;
-            
-    }
-    
-    
-    int findMaxEleIndx(vector<int>nums, int l, int r)
-    {
-        if( l > r)
-            return -1;
-        if( l == r)
-            return l;
-        int maxIndx = l;
-        
-        
-        for(int i = l; i <= r; i++)
-        {
-                if(nums[i] > nums[maxIndx])
-                {
-                    maxIndx = i;
-                }
-        }
-        
-        return maxIndx;
+        return traverse(nums.begin(), nums.end());
     }
 };
