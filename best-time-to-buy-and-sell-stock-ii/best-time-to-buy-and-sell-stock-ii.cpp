2c8ef73b81c44f933ad1144dde4f91d55bafcf90
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2021-06-24 11:25:31 +0530

    Time: 4 ms (91.78%), Space: 13 MB (84.82%) - LeetHub

--- best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -2,6 +2,9 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
         int sz = prices.size();
         int i = 0;
         int profit = 0;
