6deec51084236f6b239605aa848599554d2e0afc
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2021-07-05 21:00:29 +0530

    Time: 12 ms (66.67%), Space: 16.2 MB (33.33%) - LeetHub

---
+++ build-array-from-permutation/build-array-from-permutation.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        int sz = nums.size();
+        vector<int> res(sz);
+        for(int i = 0; i < sz; i++)
+        {
+            res[i] = nums[nums[i]];
+        }
+        
+        return res;
+    }
+};
