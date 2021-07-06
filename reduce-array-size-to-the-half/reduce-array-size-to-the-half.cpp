15813aa544c7b652ca0d574498d5f0f3ecafcee1
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2021-07-06 12:48:22 +0530

    Time: 308 ms (17.39%), Space: 99.5 MB (5.62%) - LeetHub

--- reduce-array-size-to-the-half/reduce-array-size-to-the-half.cpp
+++ reduce-array-size-to-the-half/reduce-array-size-to-the-half.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     int minSetSize(vector<int>& arr) {
+        
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+        
         map<int,int> m;
         for(auto val: arr)
         {
@@ -10,18 +15,17 @@ public:
         int originalSz = sz;
         int res = 0;
         
-        vector<pair<int,int>> t;
+        multiset<int,greater <int>> s;  
         for(auto i = m.begin(); i != m.end(); i++)
         {
-            t.push_back(make_pair(i->first,i->second));
+            s.insert(i->second);
         }   
         
-        sort(t.begin(),t.end(),comparator);
         
-        auto i = 0;
+        auto i = s.begin();
         while(sz > originalSz/2)
         {
-            sz = sz - t[i].second;
+            sz = sz - *i;
             i++;
             res++;
         }
@@ -29,9 +33,5 @@ public:
         return res;
     }
     
-    static bool comparator(pair<int,int> a , pair<int,int> b)
-    {
-        return a.second > b.second;
-        
-    }
+   
 };
