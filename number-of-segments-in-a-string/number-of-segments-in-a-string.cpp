8cd93dca5d4369bf9d2da356fa04830ffc508c53
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2021-07-10 21:54:25 +0530

    Time: 4 ms (8.16%), Space: 6.1 MB (83.46%) - LeetHub

--- number-of-segments-in-a-string/number-of-segments-in-a-string.cpp
+++ number-of-segments-in-a-string/number-of-segments-in-a-string.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     int countSegments(string s) {
+        
+        
+        
         int sz = s.size();
         int res = 0;
         int i = 0;
-        bool flag = false;
+        
         
         int count = 0;
         for(int i = 0; i < sz; )
