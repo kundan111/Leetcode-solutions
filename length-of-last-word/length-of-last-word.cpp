f7e844c53d863c3c1d0fab6e1e7d4623ab0ce6c2
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2021-07-10 11:52:58 +0530

    Time: 4 ms (65.49%), Space: 6.5 MB (80.52%) - LeetHub

---
+++ length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int res = 0;
+        int i = s.size()-1;
+        while(i >= 0 && s[i] == ' ')
+        {
+            i--;
+        }
+        while(i >= 0 && s[i] != ' ')
+        {
+            i--;
+            res++;
+        }
+        return res;
+    }
+};
