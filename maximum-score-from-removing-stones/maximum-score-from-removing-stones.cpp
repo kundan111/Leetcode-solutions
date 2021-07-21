e96f39c7941cd9646c2d76ff2f82fa79fc77122c
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2021-07-21 18:48:34 +0530

    Time: 32 ms (60.73%), Space: 6.2 MB (6.53%) - LeetHub

--- maximum-score-from-removing-stones/maximum-score-from-removing-stones.cpp
+++ maximum-score-from-removing-stones/maximum-score-from-removing-stones.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
     int maximumScore(int a, int b, int c) {
+        
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+
+        
         vector<int> v{a,b,c};
         int res = 0;
         while( (v[0] && v[1]) || (v[1] && v[2]) || (v[0] && v[2]))
         {
         
             res++;
-            sort(v.begin(),v.end());
+            // sort(v.begin(),v.end());
+            cusSort(v);
             v[1]--;
             v[2]--;
         }
@@ -17,5 +24,19 @@ public:
         
     }
     
+    void cusSort(vector<int> &arr)
+    {
+        // Insert arr[1]
+        if (arr[1] < arr[0])
+           swap(arr[0], arr[1]);
+
+        // Insert arr[2]
+        if (arr[2] < arr[1])
+        {
+           swap(arr[1], arr[2]);
+           if (arr[1] < arr[0])
+              swap(arr[1], arr[0]);
+        }
+    }
    
 };
