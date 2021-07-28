7a33470b02f43a3680460a0175630b95ab363ae8
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2021-07-28 16:10:24 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (42.58%) - LeetHub

--- check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
+++ check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
+        
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+
+        
         bitset<26> s;
         int i = 0;
         int sz = sentence.size();
