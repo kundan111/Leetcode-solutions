077576f1a459a42b407be616a6bf7ff54699d140
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2021-07-28 13:54:35 +0530

    Time: 4 ms (64.63%), Space: 8.3 MB (21.91%) - LeetHub

--- unique-number-of-occurrences/unique-number-of-occurrences.cpp
+++ unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
+        
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+
+        
         unordered_map<int,int>m;
         for(int val:arr)
         {
             m[val]++;
         }
         int sz = m.size();
-        set<int>s;
+        unordered_set<int> s;
         for(auto i = m.begin(); i != m.end(); i++)
         {
+            if(s.find(i->second) != s.end())
+            {
+                return false;
+            }
             s.insert(i->second);
         }
         
