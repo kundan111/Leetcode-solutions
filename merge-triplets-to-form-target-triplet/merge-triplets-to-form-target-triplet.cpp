3dabbb523dc95bd75a7d697070bc5b989b1c63da
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2021-07-30 12:53:04 +0530

    Time: 392 ms (99.82%), Space: 147 MB (37.44%) - LeetHub

--- merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.cpp
+++ merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.cpp
@@ -6,16 +6,20 @@ public:
         cin.tie(nullptr);
         cout.tie(nullptr);
         
+
+        
+        
         vector<int> res(3,0);
         for(auto val: triplets)
         {
             if(val[0] <= target[0] && val[1] <= target[1] && val[2] <= target[2])
             {
                 res = {max(res[0],val[0]),max(res[1],val[1]),max(res[2],val[2])};
+                if(res == target)
+                    return true;
             }
         }
         
         return res == target;
     }
-    
 };
