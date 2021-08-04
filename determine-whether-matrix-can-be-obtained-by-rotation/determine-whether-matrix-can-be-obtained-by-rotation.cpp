0008e73c43178eafb98d53f1e85f52c1d96a1bac
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2021-08-04 17:25:02 +0530

    Time: 8 ms (53.97%), Space: 11.3 MB (27.88%) - LeetHub

--- determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.cpp
+++ determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
+        
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+
+        
+        
         int row = mat.size();
         int col = mat[0].size();
         
         if(target == mat)
             return true;
         
-        vector<vector<int>> temp = mat;
+        
+        vector<vector<int>> temp(row,vector<int> (col, 0));
         for(int i = 0; i < 3; i++)
         {
             
