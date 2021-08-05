4866cc5070c9d658efcb1fc52bec7cce9f94a0d0
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2021-08-05 14:51:40 +0530

    Time: 0 ms (100.00%), Space: 6 MB (54.84%) - LeetHub

--- check-if-word-equals-summation-of-two-words/check-if-word-equals-summation-of-two-words.cpp
+++ check-if-word-equals-summation-of-two-words/check-if-word-equals-summation-of-two-words.cpp
@@ -1,48 +1,24 @@
 class Solution {
 public:
     bool isSumEqual(string firstWord, string secondWord, string targetWord) {
-     
-        long long f = stringToInt(firstWord);
-        long long s = stringToInt(secondWord);
-        long long t = stringToInt(targetWord);
+        int f = convert(firstWord);
+        int s = convert(secondWord);
+        int t = convert(targetWord);
         
-        if(f > 0 && s > 0 && t > 0)
-        {
-            if(t < f || t < s)
-            {
-                return false;
-            }
-        }
-        
-        return f + s == t;
-        
-        
+        return f+s == t;
     }
     
-    long long int stringToInt(string s)
+    int convert(string s)
     {
-        long long int res = 0;
-        string digitString = "";
-        
+        int res = 0;
         for(char c: s)
         {
-            digitString += to_string(c-'a');
-        }
-        
-        int sz = digitString.size();
-        
-        res = (digitString[sz-1]-'0')%10;
-        
-        int i = sz-2;
-        
-        int j = 1;
-        while(i >= 0)
-        {
-            res = pow(10,j) * (digitString[i] - '0') + res;
-            j++;
-            i--;
+            res = res*10 + (c-'a');
         }
         
         return res;
     }
+    
+    
+    
 };
