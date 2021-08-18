04b04d25b3bf4cb60f92059e1f3c9baa98b911d3
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2021-08-18 14:59:54 +0530

    Time: 64 ms (59.06%), Space: 23.6 MB (68.87%) - LeetHub

--- count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
+++ count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
@@ -4,49 +4,32 @@ public:
         int row =  m.size();
         int col =  m[0].size();
         
-        int maxSqSize = min(row,col);
-        vector<int> res(maxSqSize+1,0);
+        int res = 0;
         
         for(int i = 0; i < row; i++)
         {
             for(int j = 0; j < col; j++)
             {
-                if(m[i][j])
+                if( i == 0 || j == 0)
                 {
-                    for(int sz = 1 ; sz <= maxSqSize; sz++)
+                    res += m[i][j];
+                }else{
+                    if(m[i][j])
                     {
-                        if(isSquare(m,i,j,row,col,sz))
-                        {
-                            res[sz]++;    
-                        }
+                        int val = min(m[i-1][j],min(m[i][j-1],m[i-1][j-1])) + 1;
+                        m[i][j] = val;
                         
-                        
+                        res += val;
                     }
                 }
             }
         }
         
-        return accumulate(res.begin(),res.end(),0);
+        
+        return res;
+        
+        
     }
     
-    bool isSquare(vector<vector<int>>& m,int i, int j, int row, int col, int sz)
-    {
-        if(( i < 0 || i >= row) || (j < 0 || j >= col))
-            return false;
-        bool checkBound = ((i + sz-1 < row) && (j + sz-1 < col));
-        
-        if(!checkBound)
-            return false;
-        
-        for(int s = i; s <= i + sz-1 ; s++)
-        {
-            for(int e = j ; e <= j + sz -1; e++)
-            {
-                if(m[s][e] != 1)
-                    return false;
-            }
-            
-        }
-        return true;
-    }
+    
 };
