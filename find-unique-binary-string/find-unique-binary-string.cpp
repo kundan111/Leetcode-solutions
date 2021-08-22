9fb78c7463f036f9df7b6916f9c91efd9e67493e
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2021-08-22 12:04:15 +0530

    Time: 4 ms (80.00%), Space: 9.9 MB (72.00%) - LeetHub

--- find-unique-binary-string/find-unique-binary-string.cpp
+++ find-unique-binary-string/find-unique-binary-string.cpp
@@ -1,63 +1,13 @@
 class Solution {
 public:
     string findDifferentBinaryString(vector<string>& nums) {
+        string ret = "";
         int sz = nums.size();
-        map<int,int>m;
-        for(string &s: nums)
+        for(int i = 0; i < sz; i++)
         {
-            int curInt = binToInt(s);
-            m[curInt]++;
+            ret += nums[i][i] == '0' ? '1' : '0';
         }
-        
-        int s = 0;
-        int e = (2 << sz) -1;
-        
-        
-        for(auto i = m.begin(); i != m.end(); i++)
-        {
-            if(i->first != s)
-            {
-                return intToString(s,sz);
-            }
-            s++;
-        }
-        
-        return intToString(s,sz);
+        return ret;
     }
     
-    int binToInt(string &s)
-    {
-        int res = 0;
-        int len = s.size();
-        
-        int c = 0;
-        for(int i = len-1; i >= 0; i--)
-        {
-            res += (s[i] - '0')*(1 << c);
-            c++;
-        }
-        return res;
-    }
-    
-    string intToString(int i, int sz)
-    {
-        string res = "";
-        while(i)
-        {
-            int last = i%2;
-            res += to_string(last);
-            i = i/2;
-        }
-
-        reverse(res.begin(),res.end());
-        
-        int res_len = res.size();
-        if(res_len < sz)
-        {
-            res = string(sz-res_len,'0') + res;
-        }
-        
-        return res;
-
-    }
 };
