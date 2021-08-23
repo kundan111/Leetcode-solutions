e26242c9e37abc7fed315ae9c3c4d0b622306c2c
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2021-08-23 20:39:14 +0530

    Time: 24 ms (99.19%), Space: 36.8 MB (67.55%) - LeetHub

--- two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
+++ two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -1,33 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     bool findTarget(TreeNode* root, int k) {
-        vector<int> nums;
-        inorder(root, nums);
-        return findTargetInSortedArray(nums, k);
-    }
+        
+        
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
 
-private:
-    void inorder(TreeNode* node, vector<int>& nums) {
-        if (!node) return;
-        inorder(node->left, nums);
-        nums.push_back(node->val);
-        inorder(node->right, nums);
-    }
-
-    bool findTargetInSortedArray(vector<int> a, int target) {
-        for (int i = 0, j = a.size() - 1; i < j;) {
-            int sum = a[i] + a[j];
-            if (sum == target) {
+        vector<int>m;
+        inorder(root,m);
+        int sz = m.size();
+        int i = 0,j = sz-1;
+        
+        while(i < j)
+        {
+            if(m[i] + m[j] > k)
+            {
+                j--;
+            }else if(m[i] + m[j] < k)
+            {
+                i++;
+            }else{
                 return true;
             }
-            else if (sum < target) {
-                i++;
-            }
-            else {
-                j--;
-            }
         }
-
+        
+        
         return false;
+    }
+    
+    void inorder(TreeNode* root, vector<int>&m)
+    {
+        if(!root)
+            return;
+        inorder(root->left,m);
+        m.push_back(root->val);
+        inorder(root->right,m);
     }
 };
