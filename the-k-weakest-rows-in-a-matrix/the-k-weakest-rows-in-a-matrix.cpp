8fcaff1970d6b78a3e7b7b6da73f6fdeb0410add
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2021-08-28 11:19:07 +0530

    Time: 8 ms (95.33%), Space: 11.4 MB (6.03%) - LeetHub

--- the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.cpp
+++ the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.cpp
@@ -5,34 +5,53 @@ public:
         int r = mat.size();
         int c = mat[0].size();
         int i = 0;
+        
+        priority_queue<pair<int,int>,vector<pair<int,int>>,comp> pq;
+        
         for(auto row : mat)
         {
-            str_vec.push_back({strength(row,0,c-1),i});
+            pq.push({strength(row,0,c-1),i});
+            if(pq.size()>k){
+                pq.pop();
+            }
             i++;
         }
-        
-        sort(str_vec.begin(),str_vec.end(),comp);
-        
         vector <int> ret;
         
         i = 0;
+        
+        // while(!pq.empty())
+        // {
+        //     auto r = pq.top();
+        //     pq.pop();
+        //     cout << "{ " << r.first << "," << r.second << " }" << endl;
+        // }
+        
+       
         while(k)
         {
-            ret.push_back(str_vec[i].second);
+            auto val = pq.top();
+            pq.pop();
+            ret.push_back(val.second);
             k--;
-            i++;
+            
         }
         
+        reverse(ret.begin(),ret.end());
         return ret;
         
         
         
     }
     
-    static bool comp(pair<int,int>a,pair<int,int>b)
-    {
-        return a.first != b.first ? (a.first < b.first) : (a.second < b.second);
-    }
+    struct comp {
+    
+       bool operator()(pair<int,int>a,pair<int,int>b)
+        {
+            return a.first != b.first ? (a.first < b.first) : (a.second < b.second);
+        }
+        
+    };
     
     int strength(vector<int> v,int l, int r)
     {
