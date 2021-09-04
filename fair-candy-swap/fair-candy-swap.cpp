ba9dbcdf9a05375cf83bbfa27d5d4e17c5f587a5
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2021-09-04 11:23:58 +0530

    Time: 115 ms (40.61%), Space: 48.2 MB (41.47%) - LeetHub

--- fair-candy-swap/fair-candy-swap.cpp
+++ fair-candy-swap/fair-candy-swap.cpp
@@ -4,95 +4,28 @@ public:
         int s1 = accumulate(aliceSizes.begin(),aliceSizes.end(),0);
         int s2 = accumulate(bobSizes.begin(),bobSizes.end(),0);
         
+        int c = (s2-s1)/2;
         
-        vector<int>large,small;
         
-        if(s1 > s2)
+        unordered_map<int,int> m;
+        
+        for(int val: bobSizes )
         {
-            large = move(aliceSizes);
-            small = move(bobSizes);
-        }else{
-            large = move(bobSizes);
-            small = move(aliceSizes);
+            m[val]++;
         }
         
-        int c = abs((s2-s1)/2);
         
-        // y = (s2-s1)/2 + x;
-        
-        if(c == 0)
-        {
-            return {0,0};
-        }
-        
-        // cout << "small" << endl;
-        for(int val: small)
-        {
-            cout << val << " ";
-        }
-        // cout << endl;
-        // cout << "large" << endl;
-        for(int val: large)
-        {
-            cout << val << " ";
-        }
-        // cout << endl;
-        sort(small.begin(),small.end());
-        
-        int sz = small.size();
-        int l = 0;
-        
-        for(int val : large)
-        {
-            int ret = binary_search(0,sz-1,small,c,val);
-            if(ret != -1)
-            {
-                // cout << "ret: " << ret << endl << "val: " << val << endl;
-                if(s1 > s2)
-                {
-                    return {val,ret};
-                }else{
-                    return {ret,val};
-                    
-                }
+        for(int &x : aliceSizes) {
+            
+            if(m.count(c+x)){
+                return {x, c+x};
             }
+            
         }
-        
-        
-        return {-1,-1};
+        return {};
         
     }
     
     
-    int binary_search(int l, int r, vector<int> &small, int c, int curVal)
-    {
-        int sz = small.size();
-        while(l <= r)
-        {
-            int mid = l + (r-l)/2;
-            int midVal = small[mid];
-            
-            if(curVal - midVal == c)
-            {
-                // cout << endl;
-                // cout << "curVal: " << curVal << " " << "midVal: " << midVal << "c: " << c << endl;
-                // cout << "yo" << endl;
-                return midVal;
-            }
-            
-            if(midVal > curVal)
-            {
-                r = mid-1;
-            }else{
-                if(curVal - midVal < c)
-                {
-                    r = mid - 1;
-                }else{
-                    l = mid + 1;
-                }
-            }
-        }
-        
-        return -1;
-    }
+    
 };
