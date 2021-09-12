55ad00e773ec99793a7c8f0bb4705d37904c715d
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2021-09-12 18:43:27 +0530

    Time: 773 ms (28.57%), Space: 138 MB (85.71%) - LeetHub

--- number-of-pairs-of-interchangeable-rectangles/number-of-pairs-of-interchangeable-rectangles.cpp
+++ number-of-pairs-of-interchangeable-rectangles/number-of-pairs-of-interchangeable-rectangles.cpp
@@ -1,55 +1,32 @@
 class Solution {
 public:
-    long long interchangeableRectangles(vector<vector<int>>& rt) {
-        sort(rt.begin(),rt.end(),com);
-        int sz = rt.size();
+    long long interchangeableRectangles(vector<vector<int>>& rectangles) {
+        int n = rectangles.size();
+        map<pair<int, int>, int> mp;
+        long long result = 0;
         
-        
-        long long int res = 0;
-        
-        for(int i = 0; i < sz-1; i++)
-        {
-            int l = i+1;
-            int r = sz-1;
-            int curW = rt[i][0];
-            int curH = rt[i][1];
-            int temp = 0;
-            while(l <= r)
-            {
-                
-                int mid = l + (r-l)/2;
-                int midW = rt[mid][0];
-                int midH = rt[mid][1];
-                
-                long long int a = (1LL)*curW*midH;
-                long long int b = (1LL)*midW*curH;
-                
-                if(a == b)
-                {
-                    temp += (mid-l+1);
-                    l = mid+1;
-                    
-                }else if(a < b)
-                {
-                    r = mid-1;   
-                }else{
-                    l = mid+1;
-                }
-            }
+        for(int i = 0; i <n; i++) {
+            int gcd = __gcd(rectangles[i][0],rectangles[i][1]);
             
-            res += temp;
+            // cout << "gcd: " << gcd ;
             
+            //Have I seen this pair
+            if(mp.find({rectangles[i][0]/gcd, rectangles[i][1]/gcd}) != end(mp))
+                result += mp[{rectangles[i][0]/gcd, rectangles[i][1]/gcd}];
             
-            
+            // cout << " result: " << result << endl;
+
+            //Increase the count
+            mp[{rectangles[i][0]/gcd, rectangles[i][1]/gcd}]++;
         }
         
-        return res;
+        /*for(auto &val: mp)
+        {
+            cout << "(" << val.first.first << "," << val.first.second << ") " << " => " << val.second << endl;
+        }
+        */
         
-    }
-    
-    
-    static bool com(vector<int>&a,vector<int>&b)
-    {
-        return (1LL)*a[0]*b[1] <  (1LL)*b[0]*a[1];
+        
+        return result;
     }
 };
