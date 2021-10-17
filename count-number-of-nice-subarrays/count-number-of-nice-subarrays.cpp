d993c383ade4a64bfd510e1db8c0ba941dcb1a6c
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2021-10-17 22:17:38 +0530

    Time: 192 ms (48.70%), Space: 90.4 MB (20.94%) - LeetHub

--- count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,75 +1,29 @@
 class Solution {
 public:
-    int numberOfSubarrays(vector<int>& nums, int k) {
-        
-        int sz = nums.size();
-        vector<int> prefSum(sz+1);
-        
-        prefSum[0] = 0;
-        
-        for(int i = 1; i < sz+1; i++)
-        {
-            prefSum[i] = prefSum[i-1] + (nums[i-1]%2 == 1 ? 1 : 0); 
-        }
-        
-        int res = 0;
-        
-        // cout << "sz+1: " << sz+1 << endl;
-        
-        for(int i = 0; i < sz+1; i++)
-        {
-            // cout << "prefSum[i] + k: " << prefSum[i] + k << endl; 
-            int l = bs(prefSum,prefSum[i] + k,true);
-            int u = bs(prefSum,prefSum[i] + k,false);
-            
-            if(l != -1 && u != -1)
+    int numberOfSubarrays(vector<int>& a, int k) {
+        for(int &x:a)
+            x=x%2;
+        unordered_map <int,int> mp;
+        mp[0]=1;
+        int csum=0;
+        int ans=0;
+        for(int i=0;i<a.size();i++){
+            csum+=a[i];
+            if(mp.count(csum-k))
             {
-                res += u - l +1;
-                
+                // cout << "mp[" << csum-k << "]: " << mp[csum-k] << endl;
+                ans+=mp[csum-k];
             }
-            
+                
+            mp[csum]++;
+            // cout << "ans: " << ans << endl;
         }
         
-        return res;
+        // for(auto val : mp)
+        // {
+        //     cout << val.first << " -> " << val.second << endl; 
+        // }
         
-        
-        
-        
+        return ans;
     }
-    
-    int bs(vector<int>& nums, int target, bool first)
-    {
-        int l = 0;
-        int r = nums.size()-1;
-        
-        int res = -1;
-        while(l <= r)
-        {
-            int mid = l + (r-l)/2;
-            if(nums[mid] == target)
-            {
-                res = mid;
-                if(first)
-                {
-                    r = mid-1;    
-                }else{
-                    l = mid+1;
-                }
-                
-            }else if(nums[mid] > target)
-            {
-                
-                    r = mid-1;
-            }else{
-               
-               l = mid+1; 
-            }
-        }
-        
-        return res;
-        
-        
-    }
-    
-    
 };
