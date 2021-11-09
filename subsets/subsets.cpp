f38553bb1da14d9cb3ff30fd1650c542f9aab3a6
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2021-11-09 15:02:35 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (38.77%) - LeetHub

--- subsets/subsets.cpp
+++ subsets/subsets.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
-    vector<vector<int>> res;
     vector<vector<int>> subsets(vector<int>& nums) {
-        recur(nums,vector<int>(),0);
-        
+        vector<vector<int>> res;
+        recur(nums,vector<int>(),0,res);
         return res;
     }
     
-    void recur(vector<int>& nums, vector<int> temp,int indx)
+    void recur(vector<int>& nums, vector<int> temp,int indx,vector<vector<int>> &res)
     {
        res.push_back(temp);
        for(int i = indx; i < nums.size(); i++)
        {
            temp.push_back(nums[i]);
-           recur(nums,temp,i+1);
+           
+           recur(nums,temp,i+1,res);
            temp.pop_back();
        }
+    }
+    
+    void print(vector<int> &temp)
+    {
+        int sz = temp.size();
+        for(int i = 0; i < sz; i++)
+        {
+            cout << temp[i] << endl;
+        }
+        cout << endl;
     }
 };
