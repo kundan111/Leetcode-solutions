1881ceab0c101541c30217fe02bac9a79a512959
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2021-11-13 08:58:12 +0530

    Time: 52 ms (60.67%), Space: 29.5 MB (14.09%) - LeetHub

--- count-unique-characters-of-all-substrings-of-a-given-string/count-unique-characters-of-all-substrings-of-a-given-string.cpp
+++ count-unique-characters-of-all-substrings-of-a-given-string/count-unique-characters-of-all-substrings-of-a-given-string.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int uniqueLetterString(string s) {
-        int tracker[26][2];
-        int sz = s.size();
-        memset(tracker,-1, sizeof(int)*52);
-        int res = 0;
-        for(int i = 0; i < sz; i++)
-        {
-            int ind = s[i] - 'A';
-            res += (i - tracker[ind][1])*(tracker[ind][1] - tracker[ind][0]);
-            tracker[ind][0] = tracker[ind][1];
-            tracker[ind][1] = i;
-        }
-        
-        for(int i = 0; i < 26; i++)
-        {
-            res += (sz - tracker[i][1]) * (tracker[i][1] - tracker[i][0]);
-        }
-        return res;
-    }
     
+	int uniqueLetterString(string s) {
+		int n = s.length();
+		vector<long long> L(n, -1), R(n, -1);
+		vector<long long> lastL(26, -1), lastR(26, n);
+		for (int i = 0; i < n; i++){
+			L[i] = i - lastL[s[i] - 'A'];
+			lastL[s[i] - 'A'] = i;
+		}     
+		for (int i = n-1; i >= 0; i--){
+			R[i] = lastR[s[i] - 'A'] - i;
+			lastR[s[i] - 'A'] = i;
+		}   
+		int ans = 0; 
+
+		for (int i = 0; i < n; i++){
+			ans = ans + (L[i] * R[i]);
+		}
+		return ans;
+    }
 };
