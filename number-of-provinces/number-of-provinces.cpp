41eca0bad4f301802e8556421975b050819163a0
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2021-11-15 12:04:25 +0530

    Time: 24 ms (66.35%), Space: 13.8 MB (61.80%) - LeetHub

--- number-of-provinces/number-of-provinces.cpp
+++ number-of-provinces/number-of-provinces.cpp
@@ -1,3 +1,45 @@
+class UnionFind {
+public:
+    UnionFind(int sz) : root(sz), rank(sz), count(sz) {
+        for (int i = 0; i < sz; i++) {
+            root[i] = i;
+            rank[i] = 1;
+        }
+    }
+
+    int find(int x) {
+        if (x == root[x]) {
+            return x;
+        }
+        return root[x] = find(root[x]);
+    }
+
+    void unionSet(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            if (rank[rootX] > rank[rootY]) {
+                root[rootY] = rootX;
+            } else if (rank[rootX] < rank[rootY]) {
+                root[rootX] = rootY;
+            } else {
+                root[rootY] = rootX;
+                rank[rootX] += 1;
+            }
+            count--;
+        }
+    }
+
+    int getCount() {
+        return count;
+    }
+
+private:
+    vector<int> root;
+    vector<int> rank;
+    int count;
+};
+
 class Solution {
 private:
     int row,col;
@@ -6,31 +48,20 @@ public:
         row = isConnected.size();
         col = isConnected[0].size();
         
-        vector<int>visited(row, false);
         
-        int res = 0;
+        UnionFind uf(row);
         for(int i = 0; i < row; i++)
         {
-            if(!visited[i])
+            for(int j = 0; j < row; j++)
             {
-                dfs(visited, isConnected, i);
-                res++;
+                if(isConnected[i][j] == 1)
+                {
+                    uf.unionSet(i,j);
+                }
             }
         }
         
-        return res;
+        return uf.getCount();
     }
     
-    void dfs(vector<int> &visited, vector<vector<int>> &isConnected, int cur)
-    {
-        for(int i = 0; i < col; i++)
-        {
-            if(isConnected[cur][i] && !visited[i])
-            {
-                visited[i] = true;
-                dfs(visited, isConnected, i);
-            }
-            
-        }
-    }
 };
