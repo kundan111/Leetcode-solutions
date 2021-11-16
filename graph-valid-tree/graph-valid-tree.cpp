3a2589a4765e34b35e19c850457132e2a617fbb0
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2021-11-16 12:24:27 +0530

    Time: 24 ms (39.41%), Space: 13.6 MB (32.88%) - LeetHub

--- graph-valid-tree/graph-valid-tree.cpp
+++ graph-valid-tree/graph-valid-tree.cpp
@@ -1,60 +1,26 @@
-
 class Solution {
 public:
     bool validTree(int n, vector<vector<int>>& edges) {
-            
-            vector<vector<int>> adjList(n);
-            for(auto v : edges)
-            {
-                adjList[v[0]].push_back(v[1]);
-                adjList[v[1]].push_back(v[0]);
-            }
+        // if(edges.size() != n - 1) return false;
+        vector<vector<int>> g(n);
+        for(auto& e: edges){
+            g[e[0]].push_back(e[1]);
+            g[e[1]].push_back(e[0]);
+        }
         
-            /*int i = 0;
-            for(auto v : adjList)
-            {
-                cout << i << " -> ";
-                for(int r : v)
-                {
-                    cout << r << " ";
+        queue<int> q{{0}};
+        vector<int> seen(n, 0);
+        while(!q.empty()){
+            for(int sz = q.size(); sz > 0; sz--){
+                auto cur = q.front();q.pop();
+                if(seen[cur] == 1) return false;
+                seen[cur]=1;
+                for(auto nxt: g[cur]){
+                    if(!seen[nxt]) q.push(nxt);                    
                 }
-                cout << endl;
-                i++;
             }
-        */
-        
-        vector<bool> visited(n,false);
-        if (hasCycle(adjList, 0, visited, -1))
-            return false;
-        
-        
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) 
-                return false;
-        }
-        
-        return true;
-                
-    
-                
-        
-    }
-    
-    bool hasCycle(vector<vector<int>> &adjList, int u, vector<bool> &visited, int parent) {
-        visited[u] = true;
-        
-        for (int i = 0; i < adjList[u].size(); i++) {
-            int v = adjList[u][i];
             
-            if ((visited[v] && parent != v) || (!visited[v] && hasCycle(adjList, v, visited, u)))
-                return true;
         }
-        
-        return false;
+        return accumulate(begin(seen),end(seen), 0) == n;        
     }
-    
-    
-    
-    
-    
 };
