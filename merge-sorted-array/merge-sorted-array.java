d6e91423ec795e6a3e202dc364fec800e5952018
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2021-12-21 09:42:09 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (55.60%) - LeetHub

---
+++ merge-sorted-array/merge-sorted-array.java
@@ -0,0 +1,16 @@
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+        // int[] res = new int[m+n];
+        int j = 0;
+        for(int i = m; i < m+n; i++)
+        {
+            nums1[i] = nums2[j];
+            j++;
+        }
+        
+        Arrays.sort(nums1);
+        
+    }
+}
