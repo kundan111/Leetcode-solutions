10d7d5c4797329467cefc1166be80accffebfd65
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2021-12-22 07:45:22 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (54.54%) - LeetHub

--- merge-sorted-array/merge-sorted-array.java
+++ merge-sorted-array/merge-sorted-array.java
@@ -1,16 +1,56 @@
-
 class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
+    public void merge(int[] num1, int m, int[] num2, int n) {
         
-        // int[] res = new int[m+n];
-        int j = 0;
         for(int i = m; i < m+n; i++)
         {
-            nums1[i] = nums2[j];
-            j++;
+            num1[i] = Integer.MAX_VALUE;
         }
         
-        Arrays.sort(nums1);
+        int[] temp = new int[m+n];
+        
+        int i = 0;
+        int j = 0;
+        int curIndex = 0;
+        while(i < m || j < n)
+        {
+            int first, second;
+            if(i < m)
+            {
+                first = num1[i];
+            }else{
+                first = Integer.MAX_VALUE;
+            }
+            
+            if(j < n)
+            {
+                second = num2[j];
+            }else{
+                second = Integer.MAX_VALUE;
+            }
+            
+            
+            
+            // System.out.println("first: " + first);
+            // System.out.println("second: " + second);
+            
+            
+            if(first <= second)
+            {
+                temp[curIndex] = first;
+                i++;
+            }else{
+                
+                temp[curIndex] = second;
+                j++;
+            }
+            curIndex++;
+        }
+        
+        for(int k = 0; k < m+n; k++)
+        {
+            num1[k] = temp[k];
+        }
         
     }
 }
+
