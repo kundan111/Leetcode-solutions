8de6a58794a362bd7b3ec8fb3fe3264d49296152
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2022-01-17 15:59:53 +0530

    Time: 2 ms (46.93%), Space: 38.4 MB (25.63%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>(new MyComp());
+        
+        for(int stone : stones)
+        {
+            pq.add(stone);
+        }
+        
+        while(pq.size() > 1)
+        {
+            int out1 = pq.poll();
+            int out2 = pq.poll();
+            
+            if(out1 != out2)
+            {
+                pq.add(Math.abs(out1-out2));
+            }
+        }
+        
+        return pq.size() == 1 ? pq.poll() : 0;
+    }
+}
+
+class MyComp implements Comparator{
+    
+    public int compare(Object A,Object B)
+    {
+        Integer f = (Integer)A;
+        Integer s = (Integer)B;
+        
+        int a = f.intValue();
+        int b = s.intValue();
+        
+        
+        if(a < b)
+        {
+            return 1;
+        }else if(a > b)
+        {
+            return -1;
+        }else{
+            return 0;
+        }
+        
+        
+    }
+}
