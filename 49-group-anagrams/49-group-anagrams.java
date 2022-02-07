9f7fa8c5f3d2815262b4e5f4e9459ddf9eb4ff79
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2022-02-07 22:47:35 +0530

    Time: 13 ms (58.53%), Space: 56.2 MB (15.06%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.java
@@ -0,0 +1,50 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String,List<String>> m = new HashMap();
+        for(String s : strs)
+        {
+            String ori = s;
+            char[] charArr = ori.toCharArray();
+            Arrays.sort(charArr);
+            String key = String.valueOf(charArr);
+            
+            // String key = cusSort(ori);
+            if(!m.containsKey(key))
+            {
+                m.put(key,new ArrayList<>());
+            }
+            m.get(key).add(s);
+        }
+        
+        // System.out.println(m.values());
+        
+        return new ArrayList<>(m.values());
+    }
+    
+    /*
+    static String cusSort(String ori)
+    {
+        char[] cArr = new char[26];
+        int sz = ori.length();
+        for(int i = 0; i < sz; i++)
+        {
+            char c = ori.charAt(i);
+            cArr[c-'a']++;
+        }
+        
+        StringBuilder s = new StringBuilder();
+        for(int i = 0; i < 26; i++)
+        {
+            StringBuilder temp = new StringBuilder();
+            for(int j = 0; j < cArr[i]; j++)
+            {
+                temp.append(i+'a');
+            }
+            
+            s.append(temp);
+        }
+        
+        return s.toString();
+    }
+    */
+}
