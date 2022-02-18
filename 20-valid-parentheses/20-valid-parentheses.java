de047e3c7ec6c171b28a663b6cd4f4de21d6c31f
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2022-02-19 02:42:24 +0530

    Time: 3 ms (54.45%), Space: 42.3 MB (12.39%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean isValid(String s) {
+        
+        // odd length string can never be balanced
+        if((s.length() & 1)  != 0)
+        {
+            return false;
+        }
+
+        Map<Character,Character> mapper = new TreeMap<>();
+        mapper.put(')', '(');
+        mapper.put('}', '{');
+        mapper.put(']', '[');
+
+        
+        Stack<Character> st = new Stack<>();
+
+        for (Character character : s.toCharArray()) {
+
+            if(character == '(' || character == '{' || character == '[')
+            {
+                st.push(character);
+            }else{
+                if(st.isEmpty())
+                {
+                    return false;
+                }
+                if(mapper.get(character) != st.pop())
+                {
+                    return false;
+                }
+            }
+            
+        }
+
+        return st.size() == 0;
+    }
+}
