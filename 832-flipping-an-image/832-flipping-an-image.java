02ed0b64b14cf75c003614e413d17b7dd0659955
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2022-02-20 20:58:27 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (44.91%) - LeetHub

--- 832-flipping-an-image/832-flipping-an-image.java
+++ 832-flipping-an-image/832-flipping-an-image.java
@@ -1,19 +1,18 @@
 class Solution {
-    public int[][] flipAndInvertImage(int[][] image) {
+public int[][] flipAndInvertImage(int[][] image) {
         
         int edge = image.length;
 
-        for (int i = 0; i < edge; i++) {
-            flipAndInvertImageUtil(image[i]);
-        }
+        
 
         for (int i = 0; i < edge; i++) {
+            flipAndInvertImageUtil(image[i]);
             for (int j = 0; j < edge; j++) {
                 image[i][j] = image[i][j] == 0 ? 1 : 0;
             }
         }
+    
 
-        
         return image;
     }
 
