3100c95134c14e052e92db26e99ff01ca2e56da8
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2022-02-20 22:44:05 +0530

    Time: 0 ms (100.00%), Space: 48.6 MB (15.29%) - LeetHub

---
+++ 867-transpose-matrix/867-transpose-matrix.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        
+        int oriRow = matrix.length;
+        int oriCol = matrix[0].length;
+
+        int resRow = oriCol;
+        int resCol = oriRow;
+
+        int[][] res = new int[resRow][resCol];
+
+        for (int i = 0; i < resCol; i++) {
+            transposeUtilUpdateCol(matrix, res, i);
+        }
+
+        return res;
+    }
+
+    void transposeUtilUpdateCol(int[][] ori, int[][] res, int colNumber)
+    {
+        int rows = res.length;
+        for (int i = 0; i < rows; i++) {
+            res[i][colNumber] = ori[colNumber][i];
+        }
+
+        
+    }
+}
