d553fbf5365535bc9f4a41d3179171782cf7944d
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2022-03-14 11:21:37 +0530

    Time: 1 ms (99.06%), Space: 55.9 MB (14.68%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -11,30 +11,66 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        
         ListNode tempA = headA;
         ListNode tempB = headB;
 
-        // while (tempA != null && tempB != null) {
-        //     tempA = tempA.next;
-        //     tempB = tempB.next;
-        // }
 
+        int countA = 0;
+        int countB = 0;
 
-        HashMap<ListNode,Integer> hm = new HashMap<>();
-        while (tempA != null) {
-            hm.put(tempA, 1);
-            tempA = tempA.next;
-        }
-        while (tempB != null) {
-            if(hm.containsKey(tempB))
+        while (tempA != null || tempB != null) {
+            
+            if(tempA != null && tempA == tempB)
             {
-                return tempB;
+                return tempA;
             }
-            tempB = tempB.next;
+
+            if(tempA != null)
+            {
+                tempA = tempA.next;
+                countA++;
+            }
+
+            if(tempB != null)
+            {
+                tempB = tempB.next;
+                countB++;
+            }
         }
 
+        // Both are of different length
+        int diff = Math.abs(countA-countB);
 
+        ListNode longer = null;
+        ListNode shorter = null;
+
+        if(countA > countB)
+        {
+            longer = headA;
+            shorter = headB;
+        }else{
+            longer = headB;
+            shorter = headA;
+        }
+
+        int count = 0;
+        while (count != diff) {
+            longer = longer.next;
+            count++;
+        }
+
+        while (longer != null &&  shorter != null) {
+            
+            if(longer == shorter)
+            {
+                return longer;
+            }
+
+            longer = longer.next;
+            shorter = shorter.next;
+        }
+
+        
         return null;
     }
 }
