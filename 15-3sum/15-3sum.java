0b0f040270c940928f6208f298df8f80d1b027c1
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2022-03-23 23:05:36 +0530

    Time: 28 ms (72.10%), Space: 58.8 MB (55.39%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -11,38 +11,45 @@ class Solution {
         Arrays.sort(nums);
 
         int sz = nums.length;
-        Set<List<Integer>> st = new HashSet<>();
-        for (int i = 0; i < sz; i++) {
-            int target = 0-nums[i];
-            int l = i+1;
-            int r = sz-1;
+        for (int i = 0; i < sz-2; i++) {
 
-            // System.out.println("target: " + target);
-            while (l < r) {
-                if(nums[l] + nums[r] == target)
-                {
-                    List<Integer> temp = new ArrayList<>();
-                    temp.add(nums[l]);
-                    temp.add(nums[r]);
-                    temp.add(nums[i]);
-                    st.add(temp);
-                    // System.out.println("printing temp: " + temp);
-                    l++;
-                    r--;
-                }else if(nums[l] + nums[r] > target)
-                {
-                    r--;
-                }else{
-                    l++;
+            if(i == 0 || nums[i] != nums[i-1])
+            {
+                int target = 0-nums[i];
+                int l = i+1;
+                int r = sz-1;
+
+                // System.out.println("target: " + target);
+                while (l < r) {
+                    if(nums[l] + nums[r] == target)
+                    {
+                        List<Integer> temp = new ArrayList<>();
+                        temp.add(nums[l]);
+                        temp.add(nums[r]);
+                        temp.add(nums[i]);
+                        res.add(temp);
+                        
+                        int tempLeft = nums[l];
+                        int tempRight = nums[r];
+
+                        while(l < r && nums[l] == tempLeft && nums[r] == tempRight)
+                        {
+                            l++;
+                            r--;
+                        }
+
+                        // l++;
+                        // r--;
+                    }else if(nums[l] + nums[r] > target)
+                    {
+                        r--;
+                    }else{
+                        l++;
+                    }
                 }
             }
         }
 
-        for (List<Integer> list : st) {
-            res.add(list);
-        }
-        
-        
-        return res;
+        return res;   
     }
 }
