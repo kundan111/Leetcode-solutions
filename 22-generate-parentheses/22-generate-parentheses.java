922ff9d1179d4f28dc23011a39389d96c46377f7
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2022-04-09 11:10:42 +0530

    Time: 47 ms (5.01%), Space: 43.1 MB (76.77%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.java
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -6,37 +6,33 @@ class Solution {
         Set<String> res = new HashSet<>();
 
         parenthesisRecur("(", ')', res);
-        // System.out.println("===========================");
         parenthesisRecur("(", '(', res);
-        
-
-
-        // System.out.println("printing res ");
-        // System.out.println(res);
-     
      
         return res.stream().collect(Collectors.toList());
     }
-    
+
+
     void parenthesisRecur(String currentStr, char currentChar, Set<String> res)
     {
-        // System.out.println("currentStr: " + currentStr + " and currentChar: " + currentChar);
         // base case
         if(currentStr.length() == totalParan)
         {
             if(isBalanced(currentStr))
             {
                 res.add(currentStr);
-                // System.out.println("final one: " + currentStr);
             }
             return;
         }
         
         currentStr += currentChar;
+
         parenthesisRecur(currentStr, ')', res);
         parenthesisRecur(currentStr, '(', res);
 
     }
+
+
+    
     boolean isBalanced(String s)
     {
         
@@ -46,12 +42,8 @@ class Solution {
             return false;
         }
 
-        
-
         Map<Character,Character> mapper = new TreeMap<>();
         mapper.put(')', '(');
-        mapper.put('}', '{');
-        mapper.put(']', '[');
 
         
         Stack<Character> st = new Stack<>();
