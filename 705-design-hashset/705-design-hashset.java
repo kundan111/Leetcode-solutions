d58b015c61d21e017aff473ae1c593e0b47c61d7
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2022-04-21 11:26:46 +0530

    Time: 29 ms (52.79%), Space: 56.2 MB (69.53%) - LeetHub

--- 705-design-hashset/705-design-hashset.java
+++ 705-design-hashset/705-design-hashset.java
@@ -1,35 +1,55 @@
 public class MyHashSet {
-int[] hash = new int[1000001];
+    private Bucket[] bucketArray;
+    private int keyRange;
+
     public MyHashSet() {
-        for (int i = 0; i < hash.length; i++) {
-            hash[i] = -1;
-        }
+        this.keyRange = 769;
+        this.bucketArray = new Bucket[this.keyRange];
+        for (int i = 0; i < this.keyRange; ++i)
+        this.bucketArray[i] = new Bucket();
     }
-    
+
+    protected int _hash(int key) {
+        return (key % this.keyRange);
+    }
+
     public void add(int key) {
-        if(hash[key] == -1)
-        {
-            hash[key] = 1;
-        } 
+        int bucketIndex = this._hash(key);
+        this.bucketArray[bucketIndex].insert(key);
     }
-    
+
     public void remove(int key) {
-        if(hash[key] != -1)
-        {
-            hash[key] = -1;
-        }
+        int bucketIndex = this._hash(key);
+        this.bucketArray[bucketIndex].delete(key);
     }
-    
+
     public boolean contains(int key) {
-        
-        return hash[key] != -1;
+        int bucketIndex = this._hash(key);
+        return this.bucketArray[bucketIndex].exists(key);
     }
 }
 
-/**
- * Your MyHashSet object will be instantiated and called as such:
- * MyHashSet obj = new MyHashSet();
- * obj.add(key);
- * obj.remove(key);
- * boolean param_3 = obj.contains(key);
- */
+class Bucket {
+    private LinkedList<Integer> container;
+  
+    public Bucket() {
+      container = new LinkedList<Integer>();
+    }
+  
+    public void insert(Integer key) {
+      int index = this.container.indexOf(key);
+      if (index == -1) {
+        this.container.addFirst(key);
+      }
+    }
+  
+    public void delete(Integer key) {
+      this.container.remove(key);
+    }
+  
+    public boolean exists(Integer key) {
+      int index = this.container.indexOf(key);
+      return (index != -1);
+    }
+    
+  }
