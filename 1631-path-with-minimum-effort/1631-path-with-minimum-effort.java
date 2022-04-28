e31b3822f2898639c58295324e4ba91e6fcbd317
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2022-04-28 16:20:12 +0530

    Time: 45 ms (94.14%), Space: 42.7 MB (88.47%) - LeetHub

---
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -0,0 +1,77 @@
+class Solution {
+    int row;
+    int col;
+
+    public int minimumEffortPath(int[][] heights) {
+        
+
+        row = heights.length;
+        col = heights[0].length;
+
+        int l = 0;
+        int r = Integer.MAX_VALUE;
+        int ans = Integer.MAX_VALUE;
+        while (l<= r) {
+            
+            boolean[][] visited = new boolean[row][col];
+            int mid = l + (r-l)/2;
+            if(dfs(heights, 0, 0, mid, visited))
+            {
+                ans = Math.min(ans, mid);
+                r = mid-1;
+            }else{
+                l = mid+1;
+            }
+
+
+        }
+
+        return ans;
+    
+    }
+
+    boolean dfs(int[][] heights, int curX, int curY, int cur_diff, boolean[][] visited)
+    {
+
+        // System.out.println("dfs(heights, " + curX + "," + curY + "," + cur_diff + ")");
+
+        if(curX == row - 1 && curY == col - 1)
+        {
+            return true;
+        }
+        int prevVal = heights[curX][curY];
+        visited[curX][curY] = true;
+
+        boolean isPathPossible = false;
+
+        if(isWithin(curX, curY+1) && Math.abs(prevVal -  heights[curX][curY+1]) <= cur_diff && !visited[curX][curY+1])
+        {
+            isPathPossible = isPathPossible || dfs(heights, curX, curY+1, cur_diff,visited);
+        }
+
+        if(isWithin(curX+1, curY) && Math.abs( prevVal -  heights[curX+1][curY] ) <= cur_diff && !visited[curX+1][curY])
+        {
+            isPathPossible = isPathPossible || dfs(heights, curX+1, curY, cur_diff,visited);
+        }
+
+        if(isWithin(curX, curY-1) && Math.abs( prevVal -  heights[curX][curY-1] ) <= cur_diff && !visited[curX][curY-1])
+        {
+            isPathPossible = isPathPossible || dfs(heights, curX, curY-1, cur_diff,visited);
+        }
+
+        if(isWithin(curX-1, curY) && Math.abs( prevVal -  heights[curX-1][curY] ) <= cur_diff && !visited[curX-1][curY])
+        {
+            isPathPossible = isPathPossible || dfs(heights, curX-1, curY, cur_diff,visited);
+        }
+        
+
+        return isPathPossible;
+
+        
+    }
+
+    boolean isWithin(int i, int j)
+    {
+        return (i >= 0 && i < row) && (j >= 0 && j < col);
+    }
+}
