98343dab1e7f4a19296e81b658987354f3332774
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2022-05-01 12:46:18 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (49.04%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -12,23 +12,25 @@ class Solution {
             return 0;
         }
 
+        
         if(n == 1)
         {
             return 1;
         }
 
-        int[] dp = new int[n+1];
 
-        dp[0] = 0;
-        dp[1] = 1;
+        int first = 0;
+        int second = 1;
 
-        for (int i = 2; i < n+1; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+        int i = 0;
+
+        while (i != n-2) {
+            int temp = first;
+            first = second;
+            second = temp + second;
+            i++;
         }
-
-        
-        
-        return dp[n];
+        return first + second;
         
     }
 }
