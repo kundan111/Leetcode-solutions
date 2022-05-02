daa18865d36b886d43c518db7e9c988c7265be26
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2022-05-02 14:54:07 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (81.13%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,18 +1,48 @@
 class Solution {
     public int climbStairs(int n) {
         
-        int[] dp = new int[n+2];
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 2;
+//         int[] dp = new int[n+2];
+//         dp[0] = 0;
+//         dp[1] = 1;
+//         dp[2] = 2;
 
-        for (int i = 3; i < dp.length; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+//         for (int i = 3; i < dp.length; i++) {
+//             dp[i] = dp[i-1] + dp[i-2];
+//         }
+
+//         // System.out.println(Arrays.toString(dp));
+
+//         return dp[n];
+        
+        return approach2(n);
+        
+    }
+    
+    public int approach2(int n) 
+    {
+        if(n == 1)
+        {
+            return 1;
         }
 
-        // System.out.println(Arrays.toString(dp));
+        if(n == 2)
+        {
+            return 2;
+        }
 
-        return dp[n];
+
+        int first = 1;
+        int second = 2;
+
+        int i = 0;
         
+
+        while (i != n-3) {
+            int temp = first;
+            first = second;
+            second = temp + second;
+            i++;
+        }
+        return first + second;
     }
 }
