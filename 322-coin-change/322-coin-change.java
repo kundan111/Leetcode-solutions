3869c236445013ef0a02f6c08399f1a45c7af53c
Kundan Ranjan <helloiamkundanranjan35@gmail.com>
2022-05-21 13:08:36 +0530

    Time: 95 ms (12.17%), Space: 72.7 MB (7.40%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,20 +1,22 @@
 class Solution {
+    int[] coinsGlobal;
     long[] dp;
 
     public int coinChange(int[] coins, int amount) {
 
         dp = new long[amount+5];
+        coinsGlobal = coins;
 
         Arrays.fill(dp, -1);
 
-        long temp = recur(coins, amount) ;
+        long temp = recur(amount) ;
+        
         return temp == Integer.MAX_VALUE ? -1 : (int)temp;
     }
+    
 
-
-    Long recur(int[] coins, long amount)
+    Long recur(long amount)
     {
-        
         if(amount < 0)
         {
             return (long) Integer.MAX_VALUE;
@@ -32,8 +34,8 @@ class Solution {
 
         Long coinsMin = (long)Integer.MAX_VALUE;
 
-        for (int i = 0; i < coins.length; i++) {
-            coinsMin = Math.min(coinsMin, 1 + recur(coins, amount - coins[i]));
+        for (int i = 0; i < coinsGlobal.length; i++) {
+            coinsMin = Math.min(coinsMin, 1 + recur(amount - coinsGlobal[i]));
         }
 
         
